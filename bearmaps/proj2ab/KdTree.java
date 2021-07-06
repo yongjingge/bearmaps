089e23a2b4485aabee0b9657e9d3e6040be5a66f
yongjing <near711@outlook.com>
2021-07-06 17:48:47 +0800

    Kd Tree finished and tested

--- bearmaps/proj2ab/KdTree.java
+++ bearmaps/proj2ab/KdTree.java
@@ -1,2 +1,135 @@
-package bearmaps.proj2ab;public class KdTree {
+package bearmaps.proj2ab;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class KdTree implements PointSet {
+
+    private Node root;
+
+    /* nested class Node */
+    private static class Node implements Comparable<Node> {
+
+        Point point; // key
+        Node left; // also down side
+        Node right; // also up side
+        boolean XBased; // initially true
+
+        public Node(Point point) {
+            this.point = point;
+            this.left = null;
+            this.right = null;
+            this.XBased = true; // initially based on the x coordinate
+        }
+
+        public void changeComparator() {
+            XBased = !XBased;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if (XBased) {
+                return Double.compare(this.point.getX(), o.point.getX());
+            } else { // if XBased is false, we will compare based on the y coordinate.
+                return Double.compare(this.point.getY(), o.point.getY());
+            }
+        }
+    }
+
+    /* lambda function of comparator */
+    public Comparator<Node> intComparator = (Node i, Node j) -> {
+        if (i.XBased) {
+            return Double.compare(i.point.getX(), j.point.getX());
+        } else {
+            return Double.compare(i.point.getY(), j.point.getY());
+        }
+    };
+
+    /* constructor of KdTree */
+    public KdTree(List<Point> points) {
+        for (Point point : points) {
+            Node cur = new Node(point);
+            root = insert(root, cur);
+        }
+    }
+
+    /* insertion of kd tree */
+    private Node insert(Node root, Node n) {
+        if (root == null) {
+            return n;
+        }
+        if (root.point.equals(n.point)) {
+            return root;
+        }
+
+        int cmp = intComparator.compare(root, n);
+        // int cmp = root.compareTo(n);
+        if (cmp > 0) {
+            // inserted node is less than the root node
+            n.changeComparator(); // initially true, now changed into false, comparison will be conducted from depth 0 level to depth 1 level
+            root.left = insert(root.left, n); // when recursively call insert on root.left and n, n will be compared based on its y coordinate due to the 'XBased' attribute is false now
+        } else if (cmp < 0) {
+            // inserted node is greater than the root node
+            n.changeComparator();
+            root.right = insert(root.right, n);
+        } else {
+            // when either the x coordinate or the y coordinate of inserted node is equal to the root node, we treat equal items as 'greater'
+            n.changeComparator();
+            root.right = insert(root.right, n);
+        }
+        return root;
+    }
+
+    @Override
+    public Point nearest(double x, double y) {
+        Point goal = new Point(x, y);
+        Node best = root;
+        return helper(root, goal, best).point;
+    }
+
+    private Node helper(Node n, Point goal, Node best) {
+        if (n == null) {
+            return best;
+        }
+        // update the best node information whenever we find a better one
+        double bestDistance = distance(goal, best.point);
+        double curDistance = distance(goal, n.point);
+        if (Double.compare(curDistance, bestDistance) < 0) {
+            best = n;
+        }
+
+        Node goalNode = new Node(goal);
+        Node goodside, badside;
+        if (intComparator.compare(n, goalNode) < 0) {
+            goodside = n.right;
+            badside = n.left;
+        } else {
+            goodside = n.left;
+            badside = n.right;
+        }
+        best = helper(goodside, goal, best);
+        if (Math.abs(n.point.getX() - goal.getX()) < bestDistance ||
+        Math.abs(n.point.getY() - goal.getY()) < bestDistance) {
+            best = helper(badside, goal, best);
+        }
+        return best;
+    }
+
+    private double distance(Point i, Point j) {
+        double pow1 = Math.pow(i.getX() - j.getX(), 2);
+        double pow2 = Math.pow(i.getY() - j.getY(), 2);
+        return Math.sqrt(pow1 + pow2);
+    }
+
+    public static void main(String[] args) {
+        Point p1 = new Point(1.1, 2.2);
+        Point p2 = new Point(3.3, 4.4);
+        Point p3 = new Point(-2.9, 4.2);
+
+        KdTree kd = new KdTree(List.of(p1, p2, p3));
+        Point res = kd.nearest(-1.0, 4.0);
+        System.out.println("The nearest point to Point(-1.0, 4.0) is:");
+        System.out.println(res.getX());
+        System.out.println(res.getY());
+    }
 }
--- bearmaps/proj2ab/NearestPointTest.java
+++ bearmaps/proj2ab/NearestPointTest.java
@@ -1,2 +1,33 @@
-package bearmaps.proj2ab;public class NearestPointTest {
+package bearmaps.proj2ab;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/* test the result generated using two different 'nearest' methods */
+public class NearestPointTest {
+
+    @Test
+    public void testNearest() {
+        Point p1 = new Point(1.1, 2.2);
+        Point p2 = new Point(3.3, 4.4);
+        Point p3 = new Point(-2.9, 4.2);
+        Point p4 = new Point(11.3, 6);
+
+        KdTree kd = new KdTree(List.of(p1, p2, p3, p4));
+        NaivePointSet naiveSet = new NaivePointSet(List.of(p1, p2, p3, p4));
+
+        Point nearestKD = kd.nearest(-1.0, 4.0);
+        Point nearestNaive = naiveSet.nearest(-1.0, 4.0);
+        assertTrue(nearestKD.equals(nearestNaive));
+        assertEquals(p3, nearestKD);
+
+        nearestKD = kd.nearest(10.3, 5.7);
+        nearestNaive = naiveSet.nearest(10.3, 5);
+        assertTrue(nearestKD.equals(nearestNaive));
+        assertEquals(p4, nearestNaive);
+    }
 }
