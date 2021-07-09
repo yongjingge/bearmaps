734dfb52e5fb2506d4f32425b00293c7fecaf9eb
yongjing <near711@outlook.com>
2021-07-09 16:18:54 +0800

    AStarGraph comment added

--- bearmaps/hw4/AStarGraph.java
+++ bearmaps/hw4/AStarGraph.java
@@ -7,6 +7,10 @@ import java.util.List;
  * Created by hug.
  */
 public interface AStarGraph<Vertex> {
+    /* provides a list of all edges that go out from the given vertex arg to its neighbors. */
     List<WeightedEdge<Vertex>> neighbors(Vertex v);
+
+    /* provides an estimate of the number of moves to reach the goal from the start position.
+    * this estimate must be less than or equal to the correct distance. */
     double estimatedDistanceToGoal(Vertex s, Vertex goal);
 }
